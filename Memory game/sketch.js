2e598ef63fbb28457c4e5312f8647a2026def80a
gandolh <gusatualex@yahoo.com>
2021-04-03 19:24:34 +0300

    some visual updates

--- Memory game/sketch.js
+++ Memory game/sketch.js
@@ -1,4 +1,8 @@
-function setup(){
-	createCanvas(250,250)
-	background(51);
+
+let main_board=document.getElementsByClassName('main_board')[0];
+for(let i=1;i<=16;i++){
+	let card=document.createElement('div');
+	card.className='card';
+	main_board.appendChild(card)
+
 }
