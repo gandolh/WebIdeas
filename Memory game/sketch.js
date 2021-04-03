8aec397531230f8ed20550182700140d527a6082
gandolh <gusatualex@yahoo.com>
2021-04-04 01:18:55 +0300

    final update?

--- Memory game/sketch.js
+++ Memory game/sketch.js
@@ -9,13 +9,17 @@ let colorByIndex = [0, '#581b98', '#fc5185', '#f4fa9c', '#17b978',
     '#1e2a78', '#ffb174', '#40514e', '#f5b5fc'
 ]
 let main_board, cards, play_button;
-
+let time_start, moves;
+let resolved_cards=0;
+let time_spent, movess;
 function setup() {
     noCanvas();
     initializeData();
     initializeBoard();
     playAndResetButton();
+    timeAndMoves();
     gameplayMemoryCard();
+	finish_game()
 }
 
 
@@ -81,14 +85,31 @@ function playAndResetButton() {
     play_button = document.getElementsByClassName('fa-play')[0];
     play_button.addEventListener('click', () => {
         gameIsOn = true;
+        time_start = Date.now();
+        moves = 0;
         removeFromClassName(document.getElementsByClassName('consoleButtons')[0], 'hidden');
-        play_button.className += ' hidden';
+        play_button.className += 'hidden ';
         for (let card of cards) {
-            card.className += 'unsolved spinning';
+            card.className += 'unsolved spinning ';
         }
         setTimeout(() => {
             removeFromClassName(main_board, 'spinning')
         }, 1000);
+    })
+    reset_button = document.getElementsByClassName('fa-redo')[0];
+    reset_button.addEventListener('click', () => {
+        gameIsOn = false;
+        for (let elem of document.getElementsByClassName('consoleButtons')[0].children) {
+            elem.className += 'hidden '
+        }
+        removeFromClassNameSO(play_button, 'hidden');
+        for (let card of cards) {
+            card.className += 'spinning '
+            setTimeout(() => {
+                removeFromClassNameSO(card, 'spinning');
+            }, 1000);
+            removeFromClassNameSO(card, 'unsolved');
+        }
     })
 }
 
@@ -98,18 +119,20 @@ function gameplayMemoryCard() {
         cards[i - 1].addEventListener('click', () => {
             if (gameIsOn == true && existCN(cards[i - 1], 'unsolved') != -1) {
                 cards[i - 1].className += 'spinning ';
-                console.log(cards[i - 1].className)
+                // console.log(cards[i - 1].className)
                 removeFromClassNameSO(cards[i - 1], 'unsolved') //single object
                     // console.log(cards[i - 1].className)
                 cards[i - 1].style.backgroundColor = colorByIndex[cards[i - 1].children[0].innerHTML];
                 if (lastPick.a == undefined) lastPick.a = cards[i - 1];
                 else if (lastPick.b == undefined) {
+                    moves++;
                     lastPick.b = cards[i - 1];
                     aInnerValue = lastPick.a.children[0].innerHTML;
                     bInnerValue = lastPick.b.children[0].innerHTML;
                     if (aInnerValue == bInnerValue) {
                         lastPick.a = undefined;
                         lastPick.b = undefined;
+                        resolved_cards+=2;
                     } else {
                         removeFromClassNameSO(lastPick.a, 'spinning');
                         removeFromClassNameSO(lastPick.b, 'spinning');
@@ -124,4 +147,29 @@ function gameplayMemoryCard() {
             }
         })
     }
+}
+
+
+function timeAndMoves() {
+    time_spent = setInterval(() => {
+        let ts_p = document.getElementsByClassName('timeSpent')[0];
+        let seconds = (Date.now() - time_start) / 1000;
+        let minutes = Math.floor(seconds / 60);
+        seconds = Math.floor(seconds % 60);
+        ts_p.innerHTML = `${minutes} minutes ${seconds} seconds`
+    }, 1000)
+    movess = setInterval(() => {
+        document.getElementsByClassName('moves')[0].innerHTML = `${moves} moves`;
+    }, 200)
+}
+
+
+function finish_game(){
+	checkFinish=setInterval(()=>{
+		if(resolved_cards==16){
+			clearInterval(time_spent);
+			clearInterval(movess);
+			clearInterval(checkFinish);
+		}
+	},1000)
 }
