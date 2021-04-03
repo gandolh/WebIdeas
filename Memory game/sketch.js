36f5011740244df8644dd565974324bcc822d6bb
gandolh <gusatualex@yahoo.com>
2021-04-04 00:32:05 +0300

    restructured

--- Memory game/sketch.js
+++ Memory game/sketch.js
@@ -3,21 +3,52 @@ let lastPick = {
     'a': undefined,
     'b': undefined
 }
+let number_of_cards = 16;
+let orderNumber = [];
+let colorByIndex = [0, '#581b98', '#fc5185', '#f4fa9c', '#17b978',
+    '#1e2a78', '#ffb174', '#40514e', '#f5b5fc'
+]
+let main_board, cards, play_button;
 
 function setup() {
     noCanvas();
-    /* DECLARATIVE ZONE */
-    let number_of_cards = 16;
-    let orderNumber = [];
+    initializeData();
+    initializeBoard();
+    playAndResetButton();
+    gameplayMemoryCard();
+}
+
+
+function removeFromClassName(parentObj, word) {
+    let c = parentObj.children;
+    let lazy_list;
+    for (let i = 0; c[i] != undefined; i++) {
+        lazy_list = c[i].className.split(' ');
+        c[i].className = '';
+        for (name of lazy_list)
+            if (name != word && name != '') c[i].className += name + ' ';
+    }
+}
+
+function removeFromClassNameSO(simpleObj, word) {
+    let lazy_list = simpleObj.className.split(' ');
+    simpleObj.className = '';
+    for (name of lazy_list)
+        if (name != word && name != '') simpleObj.className += name + ' ';
+}
+
+
+function existCN(obj, word) {
+    return obj.className.indexOf(word)
+}
+
+function initializeData() {
     for (let i = 1; i <= number_of_cards / 2; i++) {
         orderNumber.push(i);
         orderNumber.push(i);
     } // an array with elements from 1 to 8 two times
     shuffle(orderNumber, true) // shuffle the numbers
     orderNumber.splice(0, 0, 0)
-    let colorByIndex = [0, '#581b98', '#fc5185', '#f4fa9c', '#17b978',
-        '#1e2a78', '#ffb174', '#40514e', '#f5b5fc'
-    ]
 
     // for (let i = 1; i <= number_of_cards / 2; i++) {
     // 	let c=`rgba(${random(100,255)},${random(100,255)},${random(100,255)},255)`
@@ -25,12 +56,10 @@ function setup() {
     //     colorByIndex.push(c)
     // } 
     //random no good...
+}
 
-    /*	END OF DECLARATIVE ZONE */
-    /*	
-    CREATE board style
-    */
-    let main_board = document.getElementsByClassName('main_board')[0];
+function initializeBoard() {
+    main_board = document.getElementsByClassName('main_board')[0];
     for (let i = 1; i <= number_of_cards; i++) {
         let card = document.createElement('div');
         card.className = 'card ';
@@ -42,38 +71,36 @@ function setup() {
 
     }
 
-    let cards = main_board.children;
+    cards = main_board.children;
     for (let i = 1; i <= number_of_cards; i++) {
         cards[i - 1].style.backgroundColor = colorByIndex[cards[i - 1].children[0].innerHTML];
     }
-    /* 
-    end of generating the board
-    */
-    /* 
-    starting the game
-    */
-    let play_button = document.getElementsByClassName('fa-play')[0];
+}
+
+function playAndResetButton() {
+    play_button = document.getElementsByClassName('fa-play')[0];
     play_button.addEventListener('click', () => {
-            gameIsOn = true;
-            removeFromClassName(document.getElementsByClassName('consoleButtons')[0], 'hidden');
-            play_button.className += ' hidden';
-            for (let card of cards) {
-                card.className += 'unsolved spinning';
-            }
-            setTimeout(() => {
-                removeFromClassName(main_board, 'spinning')
-            }, 1000);
-        })
-        /* end of the main buttons console*/
-        /* searching pairs*/
-        /*add a solved and unsolved class */
+        gameIsOn = true;
+        removeFromClassName(document.getElementsByClassName('consoleButtons')[0], 'hidden');
+        play_button.className += ' hidden';
+        for (let card of cards) {
+            card.className += 'unsolved spinning';
+        }
+        setTimeout(() => {
+            removeFromClassName(main_board, 'spinning')
+        }, 1000);
+    })
+}
+
+
+function gameplayMemoryCard() {
     for (let i = 1; i <= number_of_cards; i++) {
         cards[i - 1].addEventListener('click', () => {
             if (gameIsOn == true && existCN(cards[i - 1], 'unsolved') != -1) {
                 cards[i - 1].className += 'spinning ';
                 console.log(cards[i - 1].className)
                 removeFromClassNameSO(cards[i - 1], 'unsolved') //single object
-                // console.log(cards[i - 1].className)
+                    // console.log(cards[i - 1].className)
                 cards[i - 1].style.backgroundColor = colorByIndex[cards[i - 1].children[0].innerHTML];
                 if (lastPick.a == undefined) lastPick.a = cards[i - 1];
                 else if (lastPick.b == undefined) {
@@ -97,30 +124,4 @@ function setup() {
             }
         })
     }
-
-}
-
-
-
-function removeFromClassName(parentObj, word) {
-    let c = parentObj.children;
-    let lazy_list;
-    for (let i = 0; c[i] != undefined; i++) {
-        lazy_list = c[i].className.split(' ');
-        c[i].className = '';
-        for (name of lazy_list)
-            if (name != word && name!='') c[i].className += name + ' ';
-    }
-}
-
-function removeFromClassNameSO(simpleObj, word) {
-    let lazy_list = simpleObj.className.split(' ');
-    simpleObj.className = '';
-    for (name of lazy_list)
-        if (name != word && name!='') simpleObj.className += name + ' ';
-}
-
-
-function existCN(obj, word) {
-    return obj.className.indexOf(word)
 }
