1402846ce587f26179b6585df832226b02ac995d
gandolh <gusatualex@yahoo.com>
2021-04-04 17:47:13 +0300

    added wining message

--- tic-tac-toe/sketch.js
+++ tic-tac-toe/sketch.js
@@ -1,15 +1,31 @@
 let main_board = document.getElementsByClassName('main_board')[0];
 let cards = main_board.children;
-let shape_bit=4;
-if(window.innerWidth/window.innerHeight<1)shape_bit=12;
+let shape_bit = 4;
+if (window.innerWidth / window.innerHeight < 1) shape_bit = 12;
 let player1 = true;
-
+let gameIsOn;
 for (let card of cards) {
     card.addEventListener('click', () => {
-        if (card.children[0] == undefined) {
-            if (player1) drawX(card);
-            else drawO(card);
-            player1 = !player1;
+        if (gameIsOn) {
+            if (card.children[0] == undefined) {
+                if (player1) drawX(card);
+                else drawO(card);
+                player1 = !player1;
+            }
+        }
+        //if player1==false then he won
+        // console.log(player1)
+
+        gameIsOn = !checkFinish();
+        if (gameIsOn == false) {
+            setTimeout(() => {
+                main_board.style.display = 'none';
+                let winning_message = document.createElement('h1');
+                winning_message.innerHTML = `Player ${player1?2:1} won!`;
+                winning_message.className = 'winning_message';
+                document.getElementsByClassName('container')[0].appendChild(winning_message)
+
+            }, 3000)
         }
     })
 }
@@ -40,9 +56,9 @@ function drawX(obj) {
             if (i <= middlePoint.x + canvas1.width / 4) {
                 sketch.ellipse(i, j, shape_bit, shape_bit);
                 sketch.ellipse(canvas1.width - i, j, shape_bit, shape_bit);
-                i+=shape_bit/4;
-                j-=shape_bit/4;
-            }
+                i += shape_bit / 4;
+                j -= shape_bit / 4;
+            } else sketch.noLoop();
         }
     };
     new p5(instance);
@@ -66,18 +82,36 @@ function drawO(obj) {
                 'x': canvas1.width / 2,
                 'y': canvas1.height / 2
             }
-            radius=canvas1.width/4;
-            angle=0;
+            radius = canvas1.width / 4;
+            angle = 0;
         }
-        sketch.draw = ()=>{
+        sketch.draw = () => {
             if (angle <= sketch.TWO_PI) {
                 let x = middlePoint.x + radius * sketch.cos(angle)
                 let y = middlePoint.y + radius * sketch.sin(angle)
                 sketch.ellipse(x, y, shape_bit, shape_bit);
                 angle += shape_bit / 200;
-            }
+            } else sketch.noLoop();
         }
     };
     new p5(instance);
 
+}
+
+function checkFinish() {
+    let values = Object.values(cards).map(x => {
+        if (x.children[0] != undefined) return x.children[0].innerHTML;
+        else return undefined;
+    });
+    for (let i = 0; i < 3; i++) {
+        if (values[i * 3] == values[i * 3 + 1] && values[i * 3 + 1] == values[i * 3 + 2] && values[i * 3] != undefined)
+            return true;
+        if (values[0 + i] == values[3 + i] && values[3 + i] == values[6 + i] && values[0 + i] != undefined)
+            return true;
+    }
+    if (values[0] == values[4] && values[4] == values[8] && values[0] != undefined)
+        return true;
+    if (values[2] == values[4] && values[4] == values[6] && values[2] != undefined)
+        return true;
+    return false;
 }
