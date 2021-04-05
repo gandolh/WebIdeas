c82de10fa7cd63b90f91b485a4e9652feb5c5285
gandolh <gusatualex@yahoo.com>
2021-04-05 18:04:17 +0300

    real time calculation on input

--- calculator/sketch.js
+++ calculator/sketch.js
@@ -1,26 +1,36 @@
 /* inspiration desmos calculator*/
 let history = document.getElementsByClassName('calculusHistory')[0];
 let inputBody = document.getElementsByClassName('inputBody')[0];
+let final_result;
 inputBody.children[0].select();
 // history.children[0].style.backgroundColor='red';
 addToHistory(inputBody.children[0])
-
+evaluateOnModify(inputBody.children[0])
 function addToHistory(node) {
     node.addEventListener("keyup", ({
         key
     }) => {
         if (key === "Enter") {
-            let equation = history.lastElementChild.children[0].value;
-            history.lastElementChild.children[1].innerHTML = '= ' + math.evaluate(equation);
-            newInputBody = createNewInputBody();
-            history.appendChild(newInputBody)
-            last_input = history.lastElementChild.children[0];
-            last_input.select();
-            addToHistory(last_input);
+            //post it to history
+            evaluateInput();
+            if (final_result != undefined) {
+                history.lastElementChild.children[1].innerHTML = '= ' + final_result;
+                newInputBody = createNewInputBody();
+                history.appendChild(newInputBody)
+                last_input = history.lastElementChild.children[0];
+                last_input.select();
+                evaluateOnModify(newInputBody)
+                addToHistory(last_input);
+            }
+
         }
     })
 }
-
+function evaluateOnModify(node){
+    node.addEventListener('input',(event)=>{
+        evaluateInput();
+    })
+}
 function createNewInputBody() {
     let newInputBody = document.createElement('div');
     let newInput = document.createElement('input');
@@ -30,4 +40,30 @@ function createNewInputBody() {
     newInputBody.appendChild(newInput)
     newInputBody.appendChild(newResult);
     return newInputBody;
+}
+
+function validInput(equation) {
+    try {
+        math.evaluate(equation);
+    } catch (error) {
+        return 0;
+    }
+    return 1;
+}
+
+function evaluateInput() {
+    let equation = history.lastElementChild.children[0].value;
+    if (validInput(equation)) {
+        final_result=math.evaluate(equation);
+        history.lastElementChild.children[1].innerHTML = '= ' + final_result;
+    } else {
+        final_result=undefined;
+        if (history.lastElementChild.children[1].children[0] == undefined) {
+            history.lastElementChild.children[1].innerHTML = '';
+            let warning_icon = document.createElement('i');
+            warning_icon.className = 'fas fa-exclamation-triangle';
+            history.lastElementChild.children[1].appendChild(warning_icon);
+        }
+
+    }
 }
