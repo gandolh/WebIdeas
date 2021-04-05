7d24f4890d468167ff1a3d1c7cf81b28117ddecb
gandolh <gusatualex@yahoo.com>
2021-04-05 14:06:48 +0300

    kinda functional

--- calculator/sketch.js
+++ calculator/sketch.js
@@ -1,18 +1,32 @@
 /* inspiration desmos calculator*/
 
 let history = document.getElementsByClassName('calculusHistory')[0];
-
+let inputBody = document.getElementsByClassName('inputBody')[0];
+inputBody.children[0].select();
 // history.children[0].style.backgroundColor='red';
-addToHistory(history.children[0])
+addToHistory(inputBody.children[0])
 
 function addToHistory(node) {
     node.addEventListener("keyup", ({
         key
     }) => {
         if (key === "Enter") {
-            history.insertBefore(document.createElement('input'), node)
-            history.children[0].select();
-            addToHistory(history.children[0])
+            newInputBody = createNewInputBody();
+            history.appendChild(newInputBody)
+            last_input = history.lastElementChild.children[0];
+            last_input.select();
+            addToHistory(last_input);
         }
     })
+}
+
+function createNewInputBody() {
+    let newInputBody = document.createElement('div');
+    let newInput = document.createElement('input');
+    let newResult = document.createElement('div');
+    newInputBody.className = 'inputBody';
+    newResult.className = 'result';
+    newInputBody.appendChild(newInput)
+    newInputBody.appendChild(newResult);
+    return newInputBody;
 }
