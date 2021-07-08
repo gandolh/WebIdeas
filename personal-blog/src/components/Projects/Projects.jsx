27f777ce812b44d510696f834ce29ad716689b36
gandolh <gusatualex@yahoo.com>
2021-07-08 15:17:20 +0300

    scrolling through projects button works

--- personal-blog/src/components/Projects/Projects.jsx
+++ personal-blog/src/components/Projects/Projects.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import RandomCasualtiess from './RandomCasualtiess';
+import GoNextButton from './GoNextButton';
 const Projects = () => {
     return ( <>
     <RandomCasualtiess/>
-    <button className="GoNextProject" onClick={(e)=>{
-        e.preventDefault();
-        window.scrollBy({
-            top: window.innerHeight -20 , //recalculeaza sa scazi si navbarul
-            behavior: 'smooth'
-          });
-    }}><h3>Go next project</h3></button>
+    <GoNextButton/>
     <RandomCasualtiess/>
     <RandomCasualtiess/>
     </> );
