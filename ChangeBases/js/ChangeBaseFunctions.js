f10439ea96ab6d001ea53f844c4332cc79304659
gandolh <gusatualex@yahoo.com>
2021-10-18 06:34:37 +0300

    half algo implemented

--- ChangeBases/js/ChangeBaseFunctions.js
+++ ChangeBases/js/ChangeBaseFunctions.js
@@ -1,10 +1,36 @@
-var FromBase10ToBaseReal = (value,toBase)=>{
-    return FromBase10ToBaseReal_Universal(value,toBase);
 
+//schimba baza
+var ChangeBaseReal = (value,fromBase,toBase)=>{
+    let solution_str="Nu e inca implementat";
+
+
+
+    if(fromBase==10)
+    solution_str= ChangeBaseReal_FB10(value,toBase);
+
+    //fromBase^c = toBase
+    let c=Math.log(toBase) / Math.log(fromBase);
+    if(c==parseInt(c))
+        solution_str=ChangeBaseReal_FBLOG(value,fromBase,toBase,c);
+    return solution_str;
 }
 
 
-var FromBase10ToBaseReal_Universal = (value,toBase)=>{
+var changeBaseFractionary = (value,fromBase,toBase) =>{
+    let solution_str="";
+    if(fromBase==10)
+    solution_str=changeBaseFractionary_FB10(value,toBase);
+
+    //fromBase^c = toBase
+    let c=Math.log(toBase) / Math.log(fromBase);
+    if(c==parseInt(c))
+        solution_str=changeBaseFractionary_FBLOG(value,fromBase,toBase,c);
+    
+    return solution_str;
+}
+
+
+var ChangeBaseReal_FB10 = (value,toBase)=>{
     let str="";
     while(value){
         let digit = value%toBase;
@@ -17,9 +43,7 @@ var FromBase10ToBaseReal_Universal = (value,toBase)=>{
 }
 
 
-
-
-var FromBase10ToBaseFractionary = (value,toBase) =>{
+const changeBaseFractionary_FB10 = (value,toBase) =>{
     let max_depth = 10;
     let attempts=0;
     let solution_str="";
@@ -36,3 +60,46 @@ var FromBase10ToBaseFractionary = (value,toBase) =>{
 
     return solution_str;
 }
+
+
+const ChangeBaseReal_FBLOG = (value,fromBase,toBase,c)=>{
+   //fromBase^c = toBase
+   let solution_str='';
+    while(value){
+        pow=1;
+        let digit=0;
+        for(let i=0;i<c;i++){
+            digit= digit + (value%10)*pow;
+            pow*=fromBase;
+            value=parseInt(value/10);
+        }
+        if(digit>=10)
+            digit=String.fromCharCode("A".charCodeAt(0) + digit-10);
+        solution_str=digit+ solution_str;
+    }
+    return solution_str;
+}
+
+
+const changeBaseFractionary_FBLOG = (value,fromBase,toBase,c)=>{
+    //fromBase^c = toBase
+    let solution_str="";
+    while(value!=parseInt(value)){
+        value*=10;
+        pow= Math.pow(fromBase,c-1); 
+        let digit=0;
+        for(let i=0;i<c;i++){
+            digit= digit + parseInt(value%10)*pow;
+            pow= parseInt(pow/fromBase);
+            value*=10;
+        }
+        if(digit>=10)
+            digit=String.fromCharCode("A".charCodeAt(0) + digit-10);
+        solution_str= solution_str+digit;
+        value/=10;
+    }
+
+    return solution_str;
+ }
+
+ 
--- ChangeBases/js/script.js
+++ ChangeBases/js/script.js
@@ -9,28 +9,50 @@ var changeBase = ()=>{
     let toBase= parseInt(toBase_input.value);
     let value = parseFloat(nrTransform_input.value);
 
-
+    //preia partea intreaga si fractionara.
     let realPart_value= parseInt(value);
-    let fractionaryPart_value = value - realPart_value;
+    let fractionaryPart_value = getFractionaryPart( value);
 
+    //daca avem o eroare o afisam si nu mai executam nimic.
     let ok = ValidCheck(value,fromBase,toBase);
     if(!ok.succesfully)
         return ok.errorCode;
     
 
-    //value= fromBaseToBase10(value,fromBase);
-
-    let realPartTransformed = FromBase10ToBaseReal(realPart_value,toBase);
-    let fractionaryPartTransformed = FromBase10ToBaseFractionary(fractionaryPart_value,toBase);
+    //calculeaza schimbarea partii intregi a numarului.
+    let realPartTransformed = ChangeBaseReal(realPart_value,fromBase,toBase);
+    //calculeaza schimbarea partii fractionare a numarului
+    let fractionaryPartTransformed = changeBaseFractionary(fractionaryPart_value,fromBase,toBase);
     let result;
 
 
-    
+    // daca am parte reala transformata adaug-o la rezultat
+    //altfel consider-o 0.
     if(realPartTransformed)
         result = realPartTransformed;
     else result =0;
 
+    //daca exista parte fractionara adaug-o la rezultat
     if(fractionaryPartTransformed)
         result= result + ',' + fractionaryPartTransformed;
+    
+
+    //returneaza rezultatul.
     return result;
+}
+
+
+
+const getFractionaryPart =(value)=>{
+    let real= parseInt(value)
+    let p=1;
+    while(value!=parseInt(value)){
+        value*=10;
+        real*=10;
+        p*=10;
+    }
+
+    return (value-real)/p;
+
+
 }
